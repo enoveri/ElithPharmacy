5559ff87f689765e4c00b950c9a1bbd06631e9dd
enoveri <91349024+enoveri@users.noreply.github.com>
2025-06-10 13:05:37 +0300

    enhance dashboard components with memoization and add react-icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
-import './App.css'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/index.jsx";
+import "./App.css";
 
 function App() {
   return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
